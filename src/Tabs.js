a61d646c2a029cde7822c408863bf3bbc13c5942
Julieta Fredes <julietal.fredes@gmail.com>
2023-05-30 13:48:10 +0200

    tabs

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,13 +1,86 @@
+import JSUTIL from "@andresclua/jsutil";
+
 class Tabs {
-  constructor() {
+  constructor(payload) {
+    this.tabActive = payload.tabActive; //tab active by default
+    this.tabActiveClass = payload.tabActiveClass; //active class on header
+    this.tabBodyActiveClass = payload.tabBodyActiveClass; //active class on body
+    this.tabTrigger = payload.tabTrigger;
+    this.tabParent = payload.tabParent;
+    this.tabBody = payload.tabBody;
+    this.externalTrigger = payload.externalTrigger;
+    this.onChange = payload.onChange;
+
+    this.JSUTIL = new JSUTIL();
+    this.init();
     this.events();
   }
 
-  events() {
-    this.clickEvent = this.tabs();
+  init() {
+    this.setDefaultActiveTab();
   }
 
-  tabs = () => {};
+  events() {
+    this.clickEvent = this.toggleTabs();
+  }
+
+  toggleTabs = () => {
+    const handleClick = (item, tabID) => {
+      item.preventDefault();
+      // Hides all active classes
+      this.hideTab();
+      const tabBody = document.getElementById(tabID);
+      const parent = document.querySelector(`[${this.tabParent}='${tabID}']`);
+      if (tabBody && parent) {
+        this.JSUTIL.addClass(tabBody, this.tabActiveClass);
+        this.JSUTIL.addClass(parent, this.tabBodyActiveClass);
+      }
+    };
+
+    const triggers = document.querySelectorAll(`[${this.tabTrigger}]`);
+    triggers.forEach((trigger) => {
+      const tabID = trigger.getAttribute(this.tabTrigger);
+      trigger.addEventListener("click", (item) => {
+        handleClick(item, tabID);
+      });
+    });
+
+    const externalTriggers = document.querySelectorAll(
+      `[${this.externalTrigger}]`
+    );
+    externalTriggers.forEach((externalTrigger) => {
+      const tabID = externalTrigger.getAttribute(this.externalTrigger);
+      externalTrigger.addEventListener("click", (item) => {
+        handleClick(item, tabID);
+      });
+    });
+
+    if (this.onChange) this.onChange();
+  };
+
+  // Shows tab with tabActive
+  setDefaultActiveTab() {
+    this.JSUTIL.addClass(
+      document.getElementById(this.tabActive),
+      this.tabActiveClass
+    );
+    this.JSUTIL.addClass(
+      document.querySelector(`[${this.tabTrigger}="${this.tabActive}"]`),
+      this.tabBodyActiveClass
+    );
+  }
+
+  // Hides all active clases
+  hideTab() {
+    // removes class to body
+    document.querySelectorAll(`[${this.tabBody}]`).forEach((el) => {
+      this.JSUTIL.removeClass(el, this.tabActiveClass);
+    });
+    // removes class to trigger
+    document.querySelectorAll(`[${this.tabTrigger}]`).forEach((el) => {
+      this.JSUTIL.removeClass(el, this.tabBodyActiveClass);
+    });
+  }
 
   destroy() {
     this.clickEvent = {};
--- src/index.js
+++ src/index.js
@@ -6,7 +6,31 @@ class Page {
     this.events();
   }
   init() {
-    const tabs = new Tabs();
+    const tabs = new Tabs({
+      tabActive: "tab-a-1",
+      tabActiveClass: "b--tabs-a__bd__item--is-active",
+      tabBodyActiveClass: "b--tabs-a__hd__list-item__link--is-active",
+      tabTrigger: "tf-ds-tab-to-open-a",
+      tabParent: "tf-ds-tab-parent",
+      tabBody: "tf-ds-tab-body-a",
+      externalTrigger: "tf-ds-tab-external-open-a",
+      onChange: () => {
+        // do something
+      },
+    });
+
+    const tabs2 = new Tabs({
+      tabActive: "tab-b-1",
+      tabActiveClass: "b--tabs-b__bd__item--is-active",
+      tabBodyActiveClass: "b--tabs-b__hd__list-item__link--is-active",
+      tabTrigger: "tf-ds-tab-to-open-b",
+      tabParent: "tf-ds-tab-parent",
+      tabBody: "tf-ds-tab-body-b",
+      externalTrigger: "tf-ds-tab-external-open-b",
+      onChange: () => {
+        // do something
+      },
+    });
   }
   events() {}
 }
